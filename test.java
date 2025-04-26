72001571518dde2d0e3bc4bc4b68e1ccd216d29e
Tonni_._ <sadiatonni1916@gmail.com>
2025-04-26 13:47:31 +0600

    now it sucks less

--- test.java
+++ test.java
@@ -1,15 +1,21 @@
-
 import java.util.Scanner;
 
 public class test{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
 
-        for(int i = 1; i<=n; i++){
-            System.out.println(i + " ");
+        int matrx[][]= new int[3][4];
 
+        for(int i =0; i<3;i++){
+            for(int j=0; j<4;j++){
+                matrx[i][j] = sc.nextInt();
+            }
+        }
+        for (int arr[]: matrx){
+            for(int j: arr){
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
     }
 }
